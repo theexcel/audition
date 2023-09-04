9ea62ce7bddbb9302e61db6f9d9bc65cc78ac152
theexceel <xclolatunbosun@gmail.com>
2023-09-04 12:07:05 +0100

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+
+  const title = [{
+    Foundation: [
+      {
+      id: 1,
+      task: 'setup virtual office' 
+    },
+    {
+      id: 2,
+      task: 'Set mission & vision'
+    }
+  ]
+
+  }]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My startup progress</h1>
     </div>
   );
 }
