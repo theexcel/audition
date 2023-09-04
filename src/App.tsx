45a855937f6281c0b731e13447b39f8ff2505c9f
theexceel <xclolatunbosun@gmail.com>
2023-09-04 13:00:17 +0100

    project sleletal setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,99 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+type taskType = {
+  id: number,
+  task: string
+  checked: boolean
+}
+
+type itemList = {
+  category: string,
+  tasks: taskType[]
+}
 
 const App: React.FC = () => {
-
-  const title = [{
-    Foundation: [
+  const [items, setItems] = useState<itemList[]>(
+    [
       {
-      id: 1,
-      task: 'setup virtual office' 
-    },
-    {
-      id: 2,
-      task: 'Set mission & vision'
-    }
-  ]
+        category: "Foundation",
+        tasks: [
+          {
+            id: 1,
+            task: "Setup virtual office",
+            checked: false
+          },
+          {
+            id: 2,
+            task: "Set mission & vision",
+            checked: false
+          },
+          {
+            id: 3,
+            task: "Select business name",
+            checked: false,
+          },
+          {
+            id: 4,
+            task: "Buy domains",
+            checked: false,
+          },
+        ],
+      },
+      {
+        category: "Discovery",
+        tasks: [
+          {
+            id: 1,
+            task: "Create roadmap",
+            checked: false,
+          },
+          {
+            id: 2,
+            task: "Competitor analysis",
+            checked: false,
+          },
+        ],
+      },
+      {
+        category: "Delivery",
+        tasks: [
+          {
+            id: 1,
+            task: "Release marketing website",
+            checked: false,
+          },
+          {
+            id: 2,
+            task: "Release MVP",
+            checked: false
+          },
+        ],
+      },
+    
+    ]
+  )
 
-  }]
+
+
   return (
     <div className="App">
-      <h1>My startup progress</h1>
+      {
+        items.map(({category, tasks}, index) => (
+          <div>
+            <h2>{category}</h2>
+            <ul>
+              {
+                tasks.map(({id, task}) => (
+                  <li key = {id}>{task}</li>
+                ))
+              }
+            </ul>
+          </div>
+        ))
+      }
     </div>
   );
-}
+};
 
 export default App;
