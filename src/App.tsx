2a2887ed6c652cad7a4aa1fdf8c93dfe323d2e44
theexceel <xclolatunbosun@gmail.com>
2023-09-04 16:14:30 +0100

    implemented local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Icon from './checkmark.svg'
 
 type taskType = {
   id: number,
@@ -12,88 +13,120 @@ type itemList = {
   tasks: taskType[]
 }
 
+const initialData: itemList[] = [
+  {
+    category: "Foundation",
+    tasks: [
+      {
+        id: 11,
+        task: "Setup virtual office",
+        checked: false
+      },
+      {
+        id: 12,
+        task: "Set mission & vision",
+        checked: false
+      },
+      {
+        id: 13,
+        task: "Select business name",
+        checked: false,
+      },
+      {
+        id: 14,
+        task: "Buy domains",
+        checked: false,
+      },
+    ],
+  },
+  {
+    category: "Discovery",
+    tasks: [
+      {
+        id: 15,
+        task: "Create roadmap",
+        checked: false,
+      },
+      {
+        id: 16,
+        task: "Competitor analysis",
+        checked: false,
+      },
+    ],
+  },
+  {
+    category: "Delivery",
+    tasks: [
+      {
+        id: 17,
+        task: "Release marketing website",
+        checked: false,
+      },
+      {
+        id: 18,
+        task: "Release MVP",
+        checked: false
+      },
+    ],
+  },
+]
+
 const App: React.FC = () => {
-  const [items, setItems] = useState<itemList[]>(
-    [
-      {
-        category: "Foundation",
-        tasks: [
-          {
-            id: 1,
-            task: "Setup virtual office",
-            checked: false
-          },
-          {
-            id: 2,
-            task: "Set mission & vision",
-            checked: false
-          },
-          {
-            id: 3,
-            task: "Select business name",
-            checked: false,
-          },
-          {
-            id: 4,
-            task: "Buy domains",
-            checked: false,
-          },
-        ],
-      },
-      {
-        category: "Discovery",
-        tasks: [
-          {
-            id: 1,
-            task: "Create roadmap",
-            checked: false,
-          },
-          {
-            id: 2,
-            task: "Competitor analysis",
-            checked: false,
-          },
-        ],
-      },
-      {
-        category: "Delivery",
-        tasks: [
-          {
-            id: 1,
-            task: "Release marketing website",
-            checked: false,
-          },
-          {
-            id: 2,
-            task: "Release MVP",
-            checked: false
-          },
-        ],
-      },
-    
-    ]
+  const [items, setItems] = useState<itemList[]>(() => {
+    const savedData = localStorage.getItem('items');
+    return savedData ? JSON.parse(savedData) : initialData
+  }
   )
+
+  useEffect(() => {
+    localStorage.setItem('items', JSON.stringify(items))
+  }, [items])
+
+  const handleCheck = (id: number) => {
+    const updatedItems = items.map((item) => ({
+      category: item.category,
+      tasks: item.tasks.map((task) =>
+        task.id === id ? { ...task, checked: !task.checked } : task
+      ),
+    }));
+    setItems(updatedItems);
+
+  };
 
 
 
   return (
-    <div className="App">
-      {
-        items.map(({category, tasks}, index) => (
-          <div>
-            <h2>{category}</h2>
-            <ul>
-              {
-                tasks.map(({id, task}) => (
-                  <li key = {id}>{task}</li>
-                ))
-              }
-            </ul>
-          </div>
-        ))
-      }
+  <div className="App">
+    <h1>My Startup Progress</h1>
+    <div className="app-body"> 
+    {items.map(({ category, tasks }, index) => (
+      <div key={index} >
+        <div className="category-header">
+        <div>{index + 1}</div>
+          <h2>{category}</h2>
+          {tasks.every((task) => task.checked) && <img src = {Icon} />}
+        </div>
+        <ul>
+          {tasks.map(({ id, task, checked }) => (
+            <li key={id}>
+              <div className="task-item">
+                <input 
+                type="checkbox" 
+                checked={checked} 
+                onChange={() => handleCheck(id)}
+                />
+                <label>{task}</label>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    ))}
+    <button>Hi</button>
     </div>
-  );
+  </div>
+);
+
 };
 
 export default App;
